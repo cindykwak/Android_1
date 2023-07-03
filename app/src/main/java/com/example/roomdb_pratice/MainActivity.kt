4cb445ef86f4fcd091a0c18f9c2e1940eb678cda
MinTiger <orum1223@gmail.com>
2023-07-03 20:46:36 +0900

    홈엑티비티전환

--- app/src/main/java/com/example/roomdb_pratice/MainActivity.kt
+++ app/src/main/java/com/example/roomdb_pratice/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.example.roomdb_pratice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.roomdb_pratice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var Binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(Binding.root)
 
+        Binding.btnStart.setOnClickListener{
+            val intent = Intent(this,HomeActivity::class.java)
 
+            startActivity(intent)
+        }
 
     }
 }
