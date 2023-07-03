99b55c0449823cb2cdaa42154f1863e2d66860f6
MinTiger <orum1223@gmail.com>
2023-07-03 20:38:34 +0900

    start

--- app/src/main/java/com/example/roomdb_pratice/MainActivity.kt
+++ app/src/main/java/com/example/roomdb_pratice/MainActivity.kt
@@ -2,21 +2,13 @@ package com.example.roomdb_pratice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val roomDb = AppDatabase.getInstance(this)
-
-        if (roomDb != null){
-            roomDb.userDao().updateByUserId(1,"Tiger")
 
 
-            val userList = roomDb.userDao().selectAll()
-            Log.d("DB","UserList : ${userList}")
-        }
     }
 }
