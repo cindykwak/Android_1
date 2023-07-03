7baef7ff6d385e800460f1adeeca8dd1a633c780
MinTiger <orum1223@gmail.com>
2023-07-03 21:00:07 +0900

    바텀네비게이션뷰까지

--- app/src/main/java/com/example/roomdb_pratice/HomeActivity.kt
+++ app/src/main/java/com/example/roomdb_pratice/HomeActivity.kt
@@ -2,10 +2,60 @@ package com.example.roomdb_pratice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.roomdb_pratice.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var Binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        Binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(Binding.root)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(Binding.containerFragment.id, HomeFragment())
+            .commitAllowingStateLoss()
+
+        Binding.navBottom.run{
+            setOnItemSelectedListener {
+                when(it.itemId){
+                    R.id.menu_home ->{
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(Binding.containerFragment.id, HomeFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_commnunity ->{
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(Binding.containerFragment.id, CommunityFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_dongnaeinformation ->{
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(Binding.containerFragment.id, DongnaeinformationFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_accountbook ->{
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(Binding.containerFragment.id, AccountbookFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_mypage ->{
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(Binding.containerFragment.id, MypageFragment())
+                            .commitAllowingStateLoss()
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.menu_home
+
+        }
+
+
     }
 }
